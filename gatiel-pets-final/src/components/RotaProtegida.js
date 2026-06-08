b373449b6f5e36c91b44c26bb2bad23bbc42b179
Rebeca Gomes Ferreira <rebecagomes2208@gmail.com>
2026-06-07 22:39:20 -0300

    Delete gatiel-pets-final directory

--- gatiel-pets-final/src/components/RotaProtegida.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-
-function RotaProtegida({ children }) {
-  const { usuario } = useAuth();
-  return usuario ? children : <Navigate to="/login" />;
-}
-
-export default RotaProtegida;
